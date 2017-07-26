c0a1dbd72217ad3d9f158f34c6d8f4a2de84fd35
EGOIST <0x142857@gmail.com>
2017-07-26 18:24:25 +0800

    use cac 4

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const yargs = require('yargs')
+const cac = require('cac')
 const loudRejection = require('loud-rejection')
 const chalk = require('chalk')
 const update = require('update-notifier')
@@ -9,7 +9,84 @@ update({ pkg }).notify()
 
 loudRejection()
 
-const argv = yargs
+const cli = cac()
+
+cli
+  .command('*', 'Prerender your website', (input, flags) => {
+    // eslint-disable-next-line import/order
+    const readPkg = require('read-pkg-up')
+    const Server = require('../lib/Server')
+    const Crawler = require('../lib/Crawler')
+    const Writer = require('../lib/Writer')
+    const Logger = require('../lib/Logger')
+
+    const cliOptions = pick(flags, [
+      'manually',
+      'routes',
+      'wait',
+      'outDir',
+      'verbose',
+      'browser'
+    ])
+
+    if (input[0]) {
+      cliOptions.baseDir = input[0]
+    }
+
+    let server
+    let options
+    let writer
+    let logger
+
+    readPkg()
+      .then(({ pkg: userPkg = {} }) => {
+        options = Object.assign(
+          {
+            outDir: '.presite',
+            routes: ['/']
+          },
+          userPkg.presite,
+          cliOptions
+        )
+
+        if (!options.baseDir) {
+          throw new Error('Please provide the path to your SPA!')
+        }
+
+        logger = new Logger({ verbose: !options.quiet })
+
+        server = new Server({
+          baseDir: options.baseDir
+        })
+
+        writer = new Writer({
+          outDir: options.outDir
+        })
+
+        return Promise.all([server.start(), writer.copyFrom(options.baseDir)])
+      })
+      .then(() => {
+        const crawler = new Crawler({
+          hostname: server.hostname,
+          port: server.port,
+          options,
+          writer,
+          logger
+        })
+
+        return crawler.crawl()
+      })
+      .then(() => {
+        server && server.stop()
+        logger.log(`Done, check out ${chalk.yellow(options.outDir)} folder`)
+        process.exit()
+      })
+      .catch(err => {
+        console.error(err.stack)
+        server && server.stop()
+        process.exit(1)
+      })
+  })
   .option('browser', {
     desc: 'Choose a browser, can be eithor "jsdom" (default) or "chrome"'
   })
@@ -28,86 +105,8 @@ const argv = yargs
   .option('quiet', {
     desc: 'Output nothing in console'
   })
-  .help()
-  .version(pkg.version)
-  .alias({
-    V: 'vebose',
-    v: 'version',
-    h: 'help'
-  }).argv
 
-// eslint-disable-next-line import/order
-const readPkg = require('read-pkg-up')
-const Server = require('../lib/Server')
-const Crawler = require('../lib/Crawler')
-const Writer = require('../lib/Writer')
-const Logger = require('../lib/Logger')
-
-const cliOptions = pick(argv, [
-  'manually',
-  'routes',
-  'wait',
-  'outDir',
-  'verbose',
-  'browser'
-])
-if (argv._[0]) {
-  cliOptions.baseDir = argv._[0]
-}
-
-let server
-let options
-let writer
-let logger
-
-readPkg()
-  .then(({ pkg: userPkg = {} }) => {
-    options = Object.assign(
-      {
-        outDir: '.presite',
-        routes: ['/']
-      },
-      userPkg.presite,
-      cliOptions
-    )
-
-    if (!options.baseDir) {
-      throw new Error('Please provide the path to your SPA!')
-    }
-
-    logger = new Logger({ verbose: !options.quiet })
-
-    server = new Server({
-      baseDir: options.baseDir
-    })
-
-    writer = new Writer({
-      outDir: options.outDir
-    })
-
-    return Promise.all([server.start(), writer.copyFrom(options.baseDir)])
-  })
-  .then(() => {
-    const crawler = new Crawler({
-      hostname: server.hostname,
-      port: server.port,
-      options,
-      writer,
-      logger
-    })
-
-    return crawler.crawl()
-  })
-  .then(() => {
-    server && server.stop()
-    logger.log(`Done, check out ${chalk.yellow(options.outDir)} folder`)
-    process.exit()
-  })
-  .catch(err => {
-    console.error(err.stack)
-    server && server.stop()
-    process.exit(1)
-  })
+cli.parse()
 
 function pick(obj, keys) {
   return keys.reduce((res, next) => {
